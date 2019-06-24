35b32ca1088f3370a8da3647d685309e55f379a2
Robert Heffernan <robertr@webmatrix.com.au>
2019-06-25 09:12:00 +1000

    Implement all MediaService methods and add parameter checking.

--- src/Heffsoft.PecsRemote.Api/Services/MediaService.cs
+++ src/Heffsoft.PecsRemote.Api/Services/MediaService.cs
@@ -11,15 +11,27 @@ namespace Heffsoft.PecsRemote.Api.Services
     {
         private readonly IUserService userService;
         private readonly IDataContext dataContext;
+        private readonly IDataRepository<Media> mediaRepo;
 
         public MediaService(IUserService userService, IDataContext dataContext)
         {
             this.userService = userService;
             this.dataContext = dataContext;
+
+            this.mediaRepo = this.dataContext.GetRepository<Media>();
         }
 
         public Media AddMedia(String name, Guid image, Uri url)
         {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            if (String.IsNullOrWhiteSpace(name))
+                throw new ArgumentException($"{nameof(name)} is empty.");
+
+            if (url == null)
+                throw new ArgumentNullException(nameof(url));
+
             Media media = new Media()
             {
                 Name = name,
@@ -32,49 +44,90 @@ namespace Heffsoft.PecsRemote.Api.Services
                 LastUpdatedByUserId = userService.CurrentUser?.Id ?? -1
             };
 
-            IDataRepository<Media> mediaRepo = dataContext.GetRepository<Media>();
             media.Id = mediaRepo.Insert(media);
             return media;
         }
 
-        public void DeleteMedia(int id)
+        public void DeleteMedia(Int32 id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            mediaRepo.Delete(id);
         }
 
         public void DeleteMedia(Media media)
         {
-            throw new NotImplementedException();
+            if (media == null)
+                throw new ArgumentNullException(nameof(media));
+
+            if (media.Id <= 0)
+                throw new InvalidOperationException($"{nameof(media)} has an invalid id.");
+
+            mediaRepo.Delete(media.Id);
         }
 
         public void DisableMedia(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            Media media = mediaRepo.Get(id);
+            if (media != null)
+            {
+                media.Enabled = false;
+                mediaRepo.Update(media);
+            }
         }
 
         public void DisableMedia(Media media)
         {
-            throw new NotImplementedException();
+            if (media == null)
+                throw new ArgumentNullException(nameof(media));
+
+            if (media.Id <= 0)
+                throw new InvalidOperationException($"{nameof(media)} has an invalid id.");
+
+            media.Enabled = false;
+            mediaRepo.Update(media);
         }
 
         public void EnableMedia(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            Media media = mediaRepo.Get(id);
+            if (media != null)
+            {
+                media.Enabled = true;
+                mediaRepo.Update(media);
+            }
         }
 
         public void EnableMedia(Media media)
         {
-            throw new NotImplementedException();
+            if (media == null)
+                throw new ArgumentNullException(nameof(media));
+
+            if (media.Id <= 0)
+                throw new InvalidOperationException($"{nameof(media)} has an invalid id.");
+
+            media.Enabled = true;
+            mediaRepo.Update(media);
         }
 
         public Media GetMedia(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            return mediaRepo.Get(id);
         }
 
         public IEnumerable<Media> GetMedia()
         {
-            throw new NotImplementedException();
+            return mediaRepo.GetAll();
         }
     }
 }
